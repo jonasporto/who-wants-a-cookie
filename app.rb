e4ab873a1ab81339306625a4ce2ff901ed2fb27e
Jonas Porto <jporto.contato@hotmail.com>
2017-02-06 11:29:04 -0200

    creating a quick sinatra applications with redis storage

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+
+require_relative 'config/redis'
+
+COOKIES_KEY = 'cookie:domains'
+
+get '/cookies/:domain' do
+  content_type :json
+  { cookies: $redis.hget(COOKIES_KEY, params[:domain]) }.to_json
+end
+
+post '/cookies/:domain' do
+  status 201
+  $redis.hset(COOKIES_KEY, params[:domain], params[:cookies])
+end
+
+get '/test-cookies/:domain' do
+  status 201
+  $redis.hset(COOKIES_KEY, params[:domain], params[:cookies])
+end
